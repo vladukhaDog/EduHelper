f3d54144a07765fef772813ec023edb37f2f3aeb
vladukhaDog <vladic.p@gmail.com>
2021-01-08 19:20:26 +0300

    Initial Commit

---
+++ EduHelper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EduHelper
+//
+//  Created by vladukha on 08.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
