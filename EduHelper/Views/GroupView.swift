ec9181fc80fe134b24b02e5ec23599a0744d84f9
vladukhaDog <vladic.p@gmail.com>
2021-01-08 20:11:50 +0300

    переезд звонков

--- EduHelper/Views/GroupView.swift
+++ EduHelper/Views/GroupView.swift
@@ -101,3 +101,37 @@ struct GroupView: View {
 		
 	}
 }
+
+struct bells: View {
+	@Binding var update: Bool
+	var body: some View {
+		VStack
+		{
+			Text("ПН-ПТ")
+				.fontWeight(.heavy)
+			if (update) {
+				let rings = Storage.retrieve("Rings.json", from: .caches, as: RingTimes.self)
+				ForEach(rings.ring!, id: \.self) { ring in
+					HStack
+					{
+					Text(String(ring.id ?? 1))
+						.fontWeight(.heavy)
+						Divider()
+					Text(ring.StartTime ?? "")
+						Text(" -- ")
+					Text(ring.EndTime ?? "")
+					}
+					
+				}
+				}else
+				{
+					Text("")
+				}
+		}
+		.padding()
+		.overlay(
+				RoundedRectangle(cornerRadius: 17)
+					.stroke(Color.blue, lineWidth: 1)
+			)
+	}
+}
