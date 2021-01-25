0d1b60d5f5b1123d6719ce296e07da283134c3da
vladukhaDog <vladic.p@gmail.com>
2021-01-25 08:20:23 +0300

    pair view is not separate file cuz its becoming too big

---
+++ EduHelper/Views/Pair.swift
@@ -0,0 +1,66 @@
+//
+//  Pair.swift
+//  EduHelper
+//
+//  Created by vladukha on 25.01.2021.
+//
+import SwiftUI
+import Foundation
+
+struct PairSingle: View
+{
+	@State var PairOrAlt: PairOrAlt
+	@State var alt: Bool
+	var body: some View
+	{
+		let temp = (!isEvenWeek() && (PairOrAlt.altPair?.Name != ""))
+		let pair = alt ? (temp ? PairOrAlt.Pair : PairOrAlt.altPair) : (temp ? PairOrAlt.altPair : PairOrAlt.Pair)
+		VStack()
+		{
+			HStack
+			{
+				Text(pair?.PairNumber ?? "")
+					.fontWeight(.heavy)
+					.font(.title)
+				Spacer()
+				VStack
+				{
+					if (PairOrAlt.altPair?.Name != ""){
+						HStack{
+							Spacer()
+							if(isEvenWeek())
+							{
+								Text("Пара по числителю")
+									.font(.footnote)
+									.opacity(0.6)
+							}
+							else{
+								Text("Пара по знаменателю")
+									.font(.footnote)
+									.opacity(0.6)
+							}
+							
+						}
+					}
+					
+					Text(pair?.Name ?? "--------------")
+						.fontWeight(.heavy)
+				}
+				
+			}
+			Divider()
+			Text(pair?.Teacher ?? "--")
+				.fontWeight(.light)
+		}
+		.frame(minWidth: 0,
+				maxWidth: .infinity,
+				minHeight: 0,
+				alignment: .topLeading
+			)
+		.padding()
+		.overlay(
+			RoundedRectangle(cornerRadius: 14)
+				.stroke(Color.gray, lineWidth: 4)
+		)
+	}
+}
